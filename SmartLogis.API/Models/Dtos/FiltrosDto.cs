4aad6ee57c7d0a6b35ccfd9fe60fa8a7cb4e60d1
Fernando196 <fer_jose1996@hotmail.com>
2025-10-12 22:58:42 -0500

    feat: add FiltrosDto class for filter management

---
+++ SmartLogis.API/Models/Dtos/FiltrosDto.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartLogis.API.Models
+{
+    public class FiltrosDto
+    {
+        public Dictionary<string, object> Filtros { get; set; } = new();
+    }
+}
