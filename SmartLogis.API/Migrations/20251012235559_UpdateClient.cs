9375bc3d579e48d3ab4ccb54a9f0b2f73f0fb52a
Fernando196 <fer_jose1996@hotmail.com>
2025-10-12 22:58:25 -0500

    feat: update Cliente table schema with new constraints and nullable fields

---
+++ SmartLogis.API/Migrations/20251012235559_UpdateClient.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartLogis.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateClient : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "telefono",
+                table: "Cliente",
+                type: "nvarchar(12)",
+                maxLength: 12,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(12)",
+                oldMaxLength: 12);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "modificationDate",
+                table: "Cliente",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "idModificationUser",
+                table: "Cliente",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "email",
+                table: "Cliente",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(250)",
+                oldMaxLength: 250);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "telefono",
+                table: "Cliente",
+                type: "nvarchar(12)",
+                maxLength: 12,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(12)",
+                oldMaxLength: 12,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "modificationDate",
+                table: "Cliente",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "idModificationUser",
+                table: "Cliente",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "email",
+                table: "Cliente",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(250)",
+                oldMaxLength: 250,
+                oldNullable: true);
+        }
+    }
+}
